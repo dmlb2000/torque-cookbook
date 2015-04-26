f6584dacb9b4f04b034e48b10a2591c686b54e33
David Brown <dmlb2000@gmail.com>
2015-04-25 23:09:56 -0700

    add this bit too

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,48 @@
+# Authors:: David Brown
+# Copyright 2014, David Brown
 #
-# Cookbook Name:: torque_test
-# Recipe:: default
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# Copyright 2015, YOUR_COMPANY_NAME
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-# All rights reserved - Do Not Redistribute
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
+
+# Install torque common
+include_recipe 'yum-epel::default' if node['platform_family'] == 'rhel'
+
+package "torque"
+
+user = node['torque']['user']
+
+group "#{user}"
+
+user "#{user}" do
+  shell '/bin/bash'
+end
+
+# Workaround for buggy chef user
+directory "/home/#{user}" do
+  owner "#{user}"
+  group "#{user}"
+  mode 00755
+end
+
+# Set up SSH directory and config file
+directory "/home/#{user}/.ssh" do
+  owner "#{user}"
+  group "#{user}"
+  mode 00700
+end
+
+cookbook_file "/home/#{user}/.ssh/config" do
+  source 'config'
+  owner "#{user}"
+  group "#{user}"
+end
--- recipes/install.rb
+++ recipes/install.rb
@@ -1,13 +1,10 @@
 include_recipe "yum-epel" if node['platform_family'] == 'rhel'
 
 package "torque"
-
-package "torque" do
-  action :remove
-end
-
-package "torque"
 package "torque-mom"
 package "torque-server"
 package "torque-scheduler"
+package "torque-devel"
+package "torque-drmaa"
+package "torque-drmaa-devel"
 package "torque-pam"
