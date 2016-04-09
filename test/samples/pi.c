6b95bd07ef8e96aca2df8b41805fe38cb1eda3b3
David Brown <dmlb2000@gmail.com>
2016-04-08 22:34:03 -0700

    add some sample codes to run
    This is a sample code pi.c computes the value of pi randomly. Also,
    the sample submit script for the app and the hostfile used.

---
+++ test/samples/pi.c
@@ -0,0 +1,77 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <mpi.h>
+
+int main(int argc, char ** argv)
+{
+    if( MPI_Init(&argc, &argv) != MPI_SUCCESS )
+    {
+        fprintf(stderr, "MPI_Init Failed!!!\n");
+        return -1;
+    }
+
+    int world_rank = -1;
+    long int le_one = 0;
+    long int g_one = 0;
+    long int sum_le_one = 0;
+    long int sum_g_one = 0;
+
+    if( MPI_Comm_rank(MPI_COMM_WORLD, &world_rank) != MPI_SUCCESS )
+    {
+        fprintf(stderr, "MPI_Comm_rank Failed!!!\n");
+        return -1;
+    }
+
+    if (argc <= 1)
+    {
+        fprintf(stderr, "wrong usage we need an integer\n%s TIMES\n", argv[0]);
+        return -1;
+    }
+
+    char *endchk = NULL;
+    long int times = strtol(argv[1], &endchk, 0);
+    /* check the end pointer to see if the argument is valid */
+    long int cur = 0;
+    int a,b;
+    double a_p, b_p;
+    for(cur = 0; cur < times ; cur++)
+    {
+        a = rand();
+        b = rand();
+        a_p = (double) a / (double) RAND_MAX;
+        b_p = (double) b / (double) RAND_MAX;
+        if( (a_p * a_p) + (b_p * b_p) <= 1.0 )
+            le_one++;
+        else
+            g_one++;
+    }
+
+    if( MPI_Reduce(&g_one, &sum_g_one, 1, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD) != MPI_SUCCESS )
+    {
+        fprintf(stderr, "MPI_Reduce Failed!!!\n");
+        return -1;
+    }
+    if( MPI_Reduce(&le_one, &sum_le_one, 1, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD) != MPI_SUCCESS )
+    {
+        fprintf(stderr, "MPI_Reduce Failed!!!\n");
+        return -1;
+    }
+
+    printf("(%d) g_one - %d\n", world_rank, g_one);
+    printf("(%d) le_one - %d\n", world_rank, le_one);
+    if( world_rank == 0 )
+    {
+        printf("(%d) sum_le_one - %d\n", world_rank, sum_le_one);
+        printf("(%d) sum_g_one - %d\n", world_rank, sum_g_one);
+        printf("(%d) PI - %lf\n", (double)sum_le_one*4.0/((double)sum_g_one + (double)sum_le_one));
+    }
+
+    if( MPI_Finalize() != MPI_SUCCESS )
+    {
+        fprintf(stderr, "MPI_Finalize Failed!!!\n");
+        return -1;
+    }
+    return 0;
+}
