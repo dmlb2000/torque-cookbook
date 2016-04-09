04e6b3a915261b2835548d7afe83b1407dbd1d54
David Brown <dmlb2000@gmail.com>
2016-04-08 23:29:49 -0700

    foodcritic
    Hopefully this leads people to the right places and fixes foodcritic

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ license 'All rights reserved'
 description 'Installs/Configures torque'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.1'
+issues_url 'https://github.com/dmlb2000/torque-cookbook/issues'
+source_url 'https://github.com/dmlb2000/torque-cookbook'
 
 supports 'fedora', '>= 20'
 supports 'redhat', '~> 5'
